68b06fbefac059caba773ce52ccf3a5db4f8509a
machou <uzanmacho@gmail.com>
2018-12-03 14:21:20 +0200

    Batch => 1000

--- part1/tagger1.py
+++ part1/tagger1.py
@@ -13,13 +13,16 @@ USE_PRETRAINED = True if len(sys.argv) > 3 else False
 print "Using pretrained" if USE_PRETRAINED else "Not using pretrained"
 train_name = sys.argv[1]  # "data/pos/train"
 dev_name = sys.argv[2]  # "data/pos/dev"
+print "Using train: " + train_name
+print "Using dev: " + dev_name
+
 vocabFile = sys.argv[3] if USE_PRETRAINED else None
 vectorsFile = sys.argv[4] if USE_PRETRAINED else None
 
 LR = 0.01
 LR_DECAY = 0.8
 EPOCHS = 10
-BATCH_SIZE = 10000
+BATCH_SIZE = 1000
 HIDDEN_LAYER = 150
 
 words, labels = load_train(train_name)
@@ -38,6 +41,8 @@ id_label = {i: label for label, i in label_id.items()}
 
 
 def get_words_id(word, words_id=words_id):
+    if USE_PRETRAINED:
+        word = word.lower()
     if word not in words_id:
         return words_id["UUUNKKK"]
     return words_id[word]
@@ -146,6 +151,7 @@ if __name__ == '__main__':
     print('Learning rate {}'.format(LR))
     print('Learning rate decay {}'.format(LR_DECAY))
     print('Hidden layer {}'.format(HIDDEN_LAYER))
+    print('Batch size {}'.format(BATCH_SIZE))
 
     train_vecs = np.array(map(lambda (word, tag): [get_words_id(word), label_id[tag]], zip(words, labels)))
     if USE_PRETRAINED:
@@ -169,5 +175,5 @@ if __name__ == '__main__':
         train_model(model, optimizer, train_data, BATCH_SIZE)
         for g in optimizer.param_groups:
             g['lr'] = g['lr'] * LR_DECAY
-    #create_graph("POS_loss", [loss_history], make_new=True)
-    #create_graph("POS_accuracy", [accuracy_history], ylabel="Accuracy", make_new=True)
+    create_graph("POS_loss", [loss_history], make_new=True)
+    create_graph("POS_accuracy", [accuracy_history], ylabel="Accuracy", make_new=True)
